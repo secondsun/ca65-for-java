253c8c2ec362b9928e093524347e2d7340ea9b0d
Summers Pittman <secondsun@gmail.com>
2020-07-14 11:02:14 -0400

    test: added a test

---
+++ src/test/java/dev/secondsun/ca65/CA65DirectiveTests.java
@@ -0,0 +1,100 @@
+package dev.secondsun.ca65;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static dev.secondsun.ca65.CA65BasicTests.resource;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CA65DirectiveTests {
+
+    @Test
+    @DisplayName("Create a directive node as an object")
+    public void makeDirectiveNode() throws IOException {
+        final var source = CharStreams.fromStream(resource("include.s"));
+        final AtomicInteger errors = new AtomicInteger(0);
+        final AtomicReference<ca65Parser.DirectiveContext> node = new AtomicReference<>();
+
+        ca65Lexer lexer = new ca65Lexer(source);
+        lexer.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                errors.incrementAndGet();
+            }
+        });
+
+        assertEquals(0, errors.get());
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ca65Parser parser = new ca65Parser(tokens);
+        parser.setBuildParseTree(true);
+
+        ParseTree tree = parser.file(); // parse
+        // show tree in text form
+        System.out.println(tree.toStringTree(parser));
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        ParseTreeListener evalProp = new ca65BaseListener(){
+            @Override
+            public void exitDirective(ca65Parser.DirectiveContext ctx) {
+                super.exitDirective(ctx);
+                node.set(ctx);
+            }
+        };
+        walker.walk(evalProp, tree);
+
+        assertEquals("\"libSFX.i\"", node.get().arguments().getChild(0).getText());
+
+    }
+
+    @Test
+    @DisplayName("Test Enum")
+    public void testEnum() throws IOException {
+        final var source = CharStreams.fromStream(resource("enum.s"));
+        final AtomicInteger errors = new AtomicInteger(0);
+        final AtomicReference<ca65Parser.DirectiveContext> node = new AtomicReference<>();
+
+        ca65Lexer lexer = new ca65Lexer(source);
+        lexer.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                errors.incrementAndGet();
+            }
+        });
+
+        assertEquals(0, errors.get());
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ca65Parser parser = new ca65Parser(tokens);
+        parser.setBuildParseTree(true);
+
+        var tree = parser.file(); // parse
+        // show tree in text form
+        System.out.println(tree.toStringTree(parser));
+
+        var firstEnumNode = tree.line(0).directive();
+        var firstEnumDirective = firstEnumNode.multiline_directive().enum_directive();
+        assertEquals("errorcodes", firstEnumDirective.ID().getText());
+
+        var decList = firstEnumDirective.enum_body().symbol_declaration();
+        assertEquals(3, decList.size());
+
+        var secondEnumNode = tree.line(1).directive();
+        var secondEnumDirective = secondEnumNode.multiline_directive().enum_directive();
+        assertEquals(null, secondEnumDirective.ID());
+        decList = secondEnumDirective.enum_body().symbol_declaration();
+        assertEquals(6, decList.size());
+        assertEquals("-1", decList.get(0).assignment().number().getText());
+        assertEquals("EAGAIN", decList.get(5).assignment().label().getText());
+
+
+    }
+
+}
